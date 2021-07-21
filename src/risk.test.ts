3633cb46e0b7176d16d734b2b8cd691552d2fc36
Raja Soundaramourty <rajasoun@icloud.com>
2021-07-21 13:51:55 +0530

    Initial CheckIn

---
+++ src/risk.test.ts
@@ -0,0 +1,141 @@
+import risk from "./risk";
+
+describe("Threat Index Risk Rating based on Risk Score", () => {
+  describe.each`
+    score | rating
+    ${-1} | ${"undefined"}
+    ${0}  | ${"Note"}
+    ${2}  | ${"Low"}
+    ${3}  | ${"Medium"}
+    ${5}  | ${"Medium"}
+    ${6}  | ${"High"}
+    ${9}  | ${"High"}
+    ${10} | ${"undefined"}
+  `("$score", ({ score, rating }) => {
+    test.concurrent(`risk rating for score ${score} is ${rating}`, () => {
+      expect(risk.rate(score)).toBe(rating);
+    });
+  });
+});
+
+describe("Threat Index Risk criticality based on likelihood and impact", () => {
+  describe.each`
+    likelihood   | impact       | criticality
+    ${"LOW"}     | ${"LOW"}     | ${"NOTE"}
+    ${"LOW"}     | ${"MEDIUM"}  | ${"LOW"}
+    ${"MEDIUM"}  | ${"LOW"}     | ${"LOW"}
+    ${"LOW"}     | ${"HIGH"}    | ${"MEDIUM"}
+    ${"MEDIUM"}  | ${"MEDIUM"}  | ${"MEDIUM"}
+    ${"HIGH"}    | ${"LOW"}     | ${"MEDIUM"}
+    ${"HIGH"}    | ${"MEDIUM"}  | ${"HIGH"}
+    ${"MEDIUM"}  | ${"HIGH"}    | ${"HIGH"}
+    ${"HIGH"}    | ${"HIGH"}    | ${"CRITICAL"}
+    ${"Invalid"} | ${"Invalid"} | ${"undefined"}
+  `("$criticality", ({ likelihood, impact, criticality }) => {
+    test.concurrent(
+      `risk criticality for
+                      likelihood ${likelihood} and impaact ${impact}
+                      is ${criticality}`,
+      () => {
+        expect(risk.criticality(likelihood, impact)).toBe(criticality);
+      }
+    );
+  });
+});
+
+describe("Threat Index ", () => {
+  const T1 =
+    "(SL:1/M:1/O:0/S:2/ED:1/EE:1/A:1/ID:2/LC:2/LI:1/LAV:1/LAC:1/FD:1/RD:1/NC:2/PV:3)";
+  const T2 =
+    "(SL:9/M:9/O:9/S:2/ED:9/EE:9/A:9/ID:9/LC:9/LI:9/LAV:1/LAC:9/FD:1/RD:1/NC:2/PV:9)";
+  const T3 =
+    "(SL:9/M:9/O:4/S:2/ED:9/EE:3/A:6/ID:5/LC:6/LI:5/LAV:5/LAC:9/FD:3/RD:5/NC:5/PV:5)";
+  const T4 =
+    "(SL:9/M:9/O:9/S:2/ED:9/EE:9/A:9/ID:9/LC:9/LI:9/LAV:1/LAC:9/FD:9/RD:9/NC:9/PV:9)";
+  describe.each`
+    inputString | wantLikelihoodScore | wantLikelihoodLabel | wantImpactScore | wantImpactLabel | wantCriticality
+    ${T1}       | ${"1.125"}          | ${"Low"}            | ${"1.500"}      | ${"Low"}        | ${"NOTE"}
+    ${T2}       | ${"8.125"}          | ${"High"}           | ${"5.125"}      | ${"Medium"}     | ${"HIGH"}
+    ${T3}       | ${"5.875"}          | ${"Medium"}         | ${"5.375"}      | ${"Medium"}     | ${"MEDIUM"}
+    ${T4}       | ${"8.125"}          | ${"High"}           | ${"8.000"}      | ${"High"}       | ${"CRITICAL"}
+  `(
+    "$inputString",
+    ({
+      inputString,
+      wantLikelihoodScore,
+      wantLikelihoodLabel,
+      wantImpactScore,
+      wantImpactLabel,
+      wantCriticality,
+    }) => {
+      test.concurrent(
+        `Threat Index for
+                        string ${inputString}
+                        is ${wantLikelihoodScore}
+                        is ${wantLikelihoodLabel}
+                        is ${wantImpactScore}
+                        is ${wantImpactLabel}
+                        is ${wantCriticality}`,
+        () => {
+          const vector = risk.stringToVector(inputString);
+          // Check likelihood Score
+          const likelihood = risk.calculateAverage(vector.slice(0, 8));
+          expect(likelihood).toBe(wantLikelihoodScore);
+          const likelihoodLabel = risk.rate(Number(likelihood));
+          expect(likelihoodLabel).toBe(wantLikelihoodLabel);
+          // Check Impact Score
+          const impact = risk.calculateAverage(vector.slice(8, 16));
+          expect(impact).toBe(wantImpactScore);
+          const impactLabel = risk.rate(Number(impact));
+          expect(impactLabel).toBe(wantImpactLabel);
+          const criticality = risk.criticality(likelihoodLabel, impactLabel);
+          expect(criticality).toBe(wantCriticality);
+        }
+      );
+    }
+  );
+});
+
+describe("Threat Index Vector to String", () => {
+  const T1 =
+    "(SL:1/M:1/O:0/S:2/ED:1/EE:1/A:1/ID:2/LC:2/LI:1/LAV:1/LAC:1/FD:1/RD:1/NC:2/PV:3)";
+  const T2 =
+    "(SL:9/M:9/O:9/S:2/ED:9/EE:9/A:9/ID:9/LC:9/LI:9/LAV:1/LAC:9/FD:1/RD:1/NC:2/PV:9)";
+  const T3 =
+    "(SL:9/M:9/O:4/S:2/ED:9/EE:3/A:6/ID:5/LC:6/LI:5/LAV:5/LAC:9/FD:3/RD:5/NC:5/PV:5)";
+  const T4 =
+    "(SL:9/M:9/O:9/S:2/ED:9/EE:9/A:9/ID:9/LC:9/LI:9/LAV:1/LAC:9/FD:9/RD:9/NC:9/PV:9)";
+  describe.each`
+    threatVectorString 
+    ${T1}      
+  `("$threatVectorString",({threatVectorString,}) => {
+      test.concurrent(
+        `Output String for
+                        ${threatVectorString}`,() => {
+          const vector = risk.stringToVector(threatVectorString);
+          const gotString = risk.vectorToString(vector)
+          expect(gotString).toBe(threatVectorString);
+        }
+      );
+    }
+  );
+});
+
+describe("Threat Index Risk Rating Colour based on Label", () => {
+  describe.each`
+    label         | wantedColour
+    ${"CRITICAL"} | ${"rgba(255, 102, 255)"}
+    ${"HIGH"}     | ${"rgba(255, 0, 0)"}
+    ${"MEDIUM"}   | ${"rgba(255, 169, 0)"}
+    ${"LOW"}      | ${"rgba(255, 255, 0)"}
+    ${"INVALID"}  | ${"rgba(144, 238, 144)"}
+  `("$label", ({ label, wantedColour }) => {
+    test.concurrent(
+      `Colour for Label ${label}
+                              is ${wantedColour}`,
+      () => {
+        expect(risk.colour(label)).toBe(wantedColour);
+      }
+    );
+  });
+});
---
+++ src/risk.ts
@@ -0,0 +1,175 @@
+/** Threat Vector Interface */
+interface ThreatVector {
+  id: number;
+  name: string;
+  value: number;
+}
+
+/** Risk Calculator */
+class Risk {
+  threatVector: Array<ThreatVector> = [];
+
+  /**
+   * Threat Index - Vector to String
+   * Vector is converted to String for easy storage
+   * @param {Array<ThreatVector>} - Input Vector of Impact & Likelihood
+   *                                (16 parameters)
+   * @return {string } String transfornmed from input vector
+   */
+  vectorToString(vector: Array<ThreatVector>): string {
+    // remove brackets
+    let result = '(' + vector[0].name + ":" + vector[0].value
+    for (let i = 1; i < vector.length; i++) {
+      result += '/' + vector[i].name + ':' + vector[i].value
+    }
+    result += ')';
+    return result;
+  }
+
+  /**
+   * Threat Index - Input String to Vector
+   * String is converted to Vector for easy mathematical operation
+   * @param {string} inputString - Input string of Impact & Likelihood
+   *                                (16 parameters)
+   * @return {Array<string> } Vector (Array) transfornmed from input string
+   */
+  stringToVector(inputString: string): Array<ThreatVector> {
+    // Clean Gloabl Array
+    this.threatVector.splice(0, this.threatVector.length);
+    // remove brackets
+    inputString = inputString.replace("(", "").replace(")", "");
+    // convert string to vector
+    const values = inputString.split("/");
+
+    for (let i = 0; i < values.length; i++) {
+      const aux = values[i].split(":");
+      const key = aux[0].toUpperCase();
+      const value = Number(aux[1]);
+      const risk: ThreatVector = { id: i, name: key, value: value };
+      this.threatVector.push(risk);
+    }
+    return this.threatVector;
+  }
+
+  /**
+   * Threat Index - Calculate Score
+   * Given a vector of 8 values - the functions resturn average value
+   * @param {string} riskVector - Input vector of 8 parameters
+   * @return {number } returns average score
+   */
+  calculateAverage(riskVector: ThreatVector[]): string {
+    let sum = 0;
+    for (let i = 0; i < riskVector.length; i++) {
+      sum = sum + riskVector[i].value;
+    }
+    return (sum / 8.0).toFixed(3);
+  }
+
+  /**
+   * Threat Index Rating based on Score
+   * High the the score - higher the risk
+   * @param {number} score - Risk Score
+   * @return {string} Risk Rating based on score
+   */
+  rate(score: number): string {
+    switch (true) {
+      case score < 0: {
+        return "undefined";
+      }
+      case score == 0: {
+        return "Note";
+      }
+      case score < 3: {
+        return "Low";
+      }
+      case score < 6: {
+        return "Medium";
+      }
+      case score <= 9: {
+        return "High";
+      }
+      default: {
+        return "undefined";
+      }
+    }
+  }
+
+  /**
+   * Threat Index Criticality based on likelihood and impact
+   * High the the score - higher the risk
+   * @param {number} likelihood - Likelihood of occurance of the Threat
+   * @param {number} impact     - Impact when the Threat Occurs
+   * @return {string} Risk Rating based on score
+   */
+  criticality(likelihood: string, impact: string): string {
+    likelihood = likelihood.toUpperCase();
+    impact = impact.toUpperCase();
+    switch (true) {
+      case likelihood == "LOW" && impact == "LOW": {
+        return "NOTE";
+      }
+      // LOW
+      case likelihood == "LOW" && impact == "MEDIUM": {
+        return "LOW";
+      }
+      case likelihood == "MEDIUM" && impact == "LOW": {
+        return "LOW";
+      }
+      // MEDIUM
+      case likelihood == "LOW" && impact == "HIGH": {
+        return "MEDIUM";
+      }
+      case likelihood == "MEDIUM" && impact == "MEDIUM": {
+        return "MEDIUM";
+      }
+      case likelihood == "HIGH" && impact == "LOW": {
+        return "MEDIUM";
+      }
+      // HIGH
+      case likelihood == "HIGH" && impact == "MEDIUM": {
+        return "HIGH";
+      }
+      case likelihood == "MEDIUM" && impact == "HIGH": {
+        return "HIGH";
+      }
+      // CRITICAL
+      case likelihood == "HIGH" && impact == "HIGH": {
+        return "CRITICAL";
+      }
+      default: {
+        return "undefined";
+      }
+    }
+  }
+
+  /**
+   * Threat Index Criticality Colour
+   * Based on Label
+   * @param {string} label - Lable - CRITICAL, HIGH, MEDIUM, LOW
+   * @return {string} Color for the Label
+   */
+  colour(label: string): string {
+    label = label.toUpperCase();
+    const colors = [
+      "rgba(255, 102, 255)",
+      "rgba(255, 0, 0)",
+      "rgba(255, 169, 0)",
+      "rgba(255, 255, 0)",
+      "rgba(144, 238, 144)",
+    ];
+
+    switch (true) {
+      default:
+        return colors[4];
+      case label == "LOW":
+        return colors[3];
+      case label == "MEDIUM":
+        return colors[2];
+      case label == "HIGH":
+        return colors[1];
+      case label == "CRITICAL":
+        return colors[0];
+    }
+  }
+}
+export default new Risk();
