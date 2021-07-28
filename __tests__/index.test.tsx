6c1786d4fcdb4a5ab03fb63e50c06854d26ad316
jagadishkesav2 <jagadish@navadhiti.com>
2021-07-28 15:15:19 +0530

    get data from github

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -2,10 +2,20 @@ import { mount, shallow } from "enzyme";
 import React from "react";
 import SelectDropDown from '../component/select';
 import Home from '../pages/index';
+import data from '../data/risk-calculator.json'
 
 import { configure } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
+
+const dropDownData: any = [];
+data.data.map((ele: any) => {
+  ele.select.map((selectele: any) => {
+      dropDownData.push(selectele)
+  })
+})
+
+// console.log(...dropDownData);
 
 const props = {
   "name": "skill level",
@@ -52,41 +62,47 @@ const props3 = {
 };
 
 describe("With Enzyme", () => {
+  // describe.each( shallow(<SelectDropDown {dropDownData} />))
   test.concurrent('Test Threat Dropdown using shallow', () => {
     // wrapper
-    const wrapper = shallow(<SelectDropDown {...props} />);
-    wrapper
-	.find('#dropdown_SL')
-	.at(0)
-	.simulate("change", {target : { value : 'Security penetration skills (1)', name: 'Security penetration skills' }})
-  
-  expect(wrapper.find('#dropdown_SL').props().value).toBe('Security penetration skills (1)');
+    let wrapper: any;
+    dropDownData.map((propsdata: any) => {
+      wrapper = shallow(<SelectDropDown {...propsdata} />);
+    })
 
-  // wrapper 1
-  const wrapper1 = shallow(<SelectDropDown {...props1} />);
-    wrapper1
-	.find('#dropdown_M')
-	.at(0)
-	.simulate("change", {target : { value : 'Low or no reward (1)', name: 'Low or no reward' }})
+  //   wrapper
+	// .find('#dropdown_SL')
+	// .at(0)
+	// .simulate("change", {target : { value : 'Security penetration skills (1)', name: 'Security penetration skills' }})
   
-  expect(wrapper1.find('#dropdown_M').props().value).toBe('Low or no reward (1)');
+  // expect(wrapper.find('#dropdown_SL').props().value).toBe('Security penetration skills (1)');
+  })
 
-  // wrapper 2
-  const wrapper2 = shallow(<SelectDropDown {...props2} />);
-    wrapper2
-	.find('#dropdown_O')
-	.at(0)
-	.simulate("change", {target : { value : 'Full access or expensive resources required (0)', name: 'Full access or expensive resources required' }})
+  // // wrapper 1
+  // const wrapper1 = shallow(<SelectDropDown {...props1} />);
+  //   wrapper1
+	// .find('#dropdown_M')
+	// .at(0)
+	// .simulate("change", {target : { value : 'Low or no reward (1)', name: 'Low or no reward' }})
   
-  expect(wrapper2.find('#dropdown_O').props().value).toBe('Full access or expensive resources required (0)');
+  // expect(wrapper1.find('#dropdown_M').props().value).toBe('Low or no reward (1)');
 
-  // wrapper 3
-  const wrapper3 = shallow(<SelectDropDown {...props3} />);
-    wrapper3
-	.find('#dropdown_S')
-	.at(0)
-	.simulate("change", {target : { value : 'Developers, System administrators (2)', name: 'Developers, System administrators' }})
+  // // wrapper 2
+  // const wrapper2 = shallow(<SelectDropDown {...props2} />);
+  //   wrapper2
+	// .find('#dropdown_O')
+	// .at(0)
+	// .simulate("change", {target : { value : 'Full access or expensive resources required (0)', name: 'Full access or expensive resources required' }})
   
-  expect(wrapper3.find('#dropdown_S').props().value).toBe('Developers, System administrators (2)');
-});
+  // expect(wrapper2.find('#dropdown_O').props().value).toBe('Full access or expensive resources required (0)');
+
+  // // wrapper 3
+  // const wrapper3 = shallow(<SelectDropDown {...props3} />);
+  //   wrapper3
+	// .find('#dropdown_S')
+	// .at(0)
+	// .simulate("change", {target : { value : 'Developers, System administrators (2)', name: 'Developers, System administrators' }})
+  
+  // expect(wrapper3.find('#dropdown_S').props().value).toBe('Developers, System administrators (2)');
+// });
 });
--- component/select.tsx
+++ component/select.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
@@ -9,18 +9,20 @@ const SelectDropDown = (props: any)=> {
   const [vector, setVector] = React.useState([{
     SL:1,M:1,O:0,S:2,ED:1,EE:1,A:1,ID:2,LC:2,LI:1,LAV:1,LAC:1,FD:1,RD:1,NC:2,PV:3
   }])
+  const [splitData, setData] = React.useState()
 
-  // if(dropDownData != '')
-  // {
-  //   console.log(dropDownData);
-  //   let replaceBrackets = dropDownData.replace("(", "").replace(")", "");
-  //   let removeSpace = replaceBrackets.split(" ");
-    // vector.map(data => {   
-    //   data[removeSpace[0]] = parseInt(removeSpace[1]);
-    // }) 
+  if(dropDownData != '')
+  {
+    console.log(dropDownData);
+    let replaceBrackets = dropDownData.replace("(", "").replace(")", "");
+    let removeSpace = replaceBrackets.split(" ");
+    vector.map(data => {   
+      data[removeSpace[0]] = parseInt(removeSpace[1]);
+      const newData = `(${data})`
+    }) 
     
-  // }
-  // console.log(vector);
+  }
+  console.log(vector);
   
 
   const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
@@ -30,7 +32,7 @@ const SelectDropDown = (props: any)=> {
     
     
     const risk = Risk.stringToVector(`(SL:1/M:1/O:0/S:2/ED:1/EE:1/A:1/ID:2/LC:2/LI:1/LAV:1/LAC:1/FD:1/RD:1/NC:2/PV:3)`)
-    // console.log(risk);
+    console.log(risk);
     const likelihood = Risk.calculateAverage(risk.slice(0, 8));
     // console.log(likelihood);
     
