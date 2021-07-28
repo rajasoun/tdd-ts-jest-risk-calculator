abb0c5716e3c061709f1d0959f9de8eb9b475150
jagadishkesav2 <jagadish@navadhiti.com>
2021-07-28 18:50:21 +0530

    update json

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -2,6 +2,7 @@ import { mount, shallow } from "enzyme";
 import React from "react";
 import SelectDropDown from '../src/component/select';
 import Home from '../src/pages/index';
+import data from '../src/data/risk-calculator.json'
 
 import { configure } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
@@ -64,44 +65,44 @@ describe("With Enzyme", () => {
   // describe.each( shallow(<SelectDropDown {dropDownData} />))
   test.concurrent('Test Threat Dropdown using shallow', () => {
     // wrapper
-    let wrapper: any;
-    dropDownData.map((propsdata: any) => {
-      wrapper = shallow(<SelectDropDown {...propsdata} />);
-    })
-
-  //   wrapper
-	// .find('#dropdown_SL')
-	// .at(0)
-	// .simulate("change", {target : { value : 'Security penetration skills (1)', name: 'Security penetration skills' }})
+    // let wrapper: any;
+    // dropDownData.map((propsdata: any) => {
+    //   wrapper = shallow(<SelectDropDown {...propsdata} />);
+    // })
+    const wrapper = shallow(<SelectDropDown {...props} />);
+    wrapper
+	.find('#dropdown_SL')
+	.at(0)
+	.simulate("change", {target : { value : 'Security penetration skills (1)', name: 'Security penetration skills' }})
   
-  // expect(wrapper.find('#dropdown_SL').props().value).toBe('Security penetration skills (1)');
-  })
+  expect(wrapper.find('#dropdown_SL').props().value).toBe('Security penetration skills (1)');
 
-  // // wrapper 1
-  // const wrapper1 = shallow(<SelectDropDown {...props1} />);
-  //   wrapper1
-	// .find('#dropdown_M')
-	// .at(0)
-	// .simulate("change", {target : { value : 'Low or no reward (1)', name: 'Low or no reward' }})
-  
-  // expect(wrapper1.find('#dropdown_M').props().value).toBe('Low or no reward (1)');
 
-  // // wrapper 2
-  // const wrapper2 = shallow(<SelectDropDown {...props2} />);
-  //   wrapper2
-	// .find('#dropdown_O')
-	// .at(0)
-	// .simulate("change", {target : { value : 'Full access or expensive resources required (0)', name: 'Full access or expensive resources required' }})
+  // wrapper 1
+  const wrapper1 = shallow(<SelectDropDown {...props1} />);
+    wrapper1
+	.find('#dropdown_M')
+	.at(0)
+	.simulate("change", {target : { value : 'Low or no reward (1)', name: 'Low or no reward' }})
   
-  // expect(wrapper2.find('#dropdown_O').props().value).toBe('Full access or expensive resources required (0)');
+  expect(wrapper1.find('#dropdown_M').props().value).toBe('Low or no reward (1)');
 
-  // // wrapper 3
-  // const wrapper3 = shallow(<SelectDropDown {...props3} />);
-  //   wrapper3
-	// .find('#dropdown_S')
-	// .at(0)
-	// .simulate("change", {target : { value : 'Developers, System administrators (2)', name: 'Developers, System administrators' }})
+  // wrapper 2
+  const wrapper2 = shallow(<SelectDropDown {...props2} />);
+    wrapper2
+	.find('#dropdown_O')
+	.at(0)
+	.simulate("change", {target : { value : 'Full access or expensive resources required (0)', name: 'Full access or expensive resources required' }})
   
-  // expect(wrapper3.find('#dropdown_S').props().value).toBe('Developers, System administrators (2)');
-// });
+  expect(wrapper2.find('#dropdown_O').props().value).toBe('Full access or expensive resources required (0)');
+
+  // wrapper 3
+  const wrapper3 = shallow(<SelectDropDown {...props3} />);
+    wrapper3
+	.find('#dropdown_S')
+	.at(0)
+	.simulate("change", {target : { value : 'Developers, System administrators (2)', name: 'Developers, System administrators' }})
+  
+  expect(wrapper3.find('#dropdown_S').props().value).toBe('Developers, System administrators (2)');
+});
 });
--- src/component/select.tsx
+++ src/component/select.tsx
@@ -1,33 +1,31 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import Risk from '../risk'
 
 const SelectDropDown = (props: any) => {
-    const [dropDownData, setDropDownData] = useState('');
-    const [vector, setVector] = useState([{
-        SL:1,M:1,O:0,S:2,ED:1,EE:1,A:1,ID:2,LC:2,LI:1,LAV:1,LAC:1,FD:1,RD:1,NC:2,PV:3
+    const [dropDownData, setDropDownData] = React.useState('');
+    const [vector, setVector] = React.useState([{
+        TAF_SL:1,TAF_M:1,TAF_O:0,TAF_S:2,ED:1,EE:1,A:1,ID:2,LC:2,LI:1,LAV:1,LAC:1,FD:1,RD:1,NC:2,PV:3
     }])
 
-    // if(dropDownData != '')
-    // {
-    //   console.log(dropDownData);
-    //   let replaceBrackets = dropDownData.replace("(", "").replace(")", "");
-    //   let removeSpace = replaceBrackets.split(" ");
-        // vector.map(data => {   
-        //   data[removeSpace[0]] = parseInt(removeSpace[1]);
-        // }) 
-        
-    // }
-    // console.log(vector);
-  
+    if(dropDownData != '')
+    {
+    //   console.log(dropDownData);    
+      let replaceBrackets: any = dropDownData.replace(":", " ");
+    //   console.log(replaceBrackets);
+      
+      let removeSpace: any = replaceBrackets.split(" ");
+    //   console.log(removeSpace[0]);
+      
+      vector.find((data: any) => data[removeSpace[0]] = parseInt(removeSpace[1]))
+    }
+    
 
-    const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    const handleChange = (event:  React.ChangeEvent<{ value: unknown }>) => {
         // console.log(event);
-        
         setDropDownData(event.target.value as string)
-        
         
         const risk = Risk.stringToVector(`(SL:1/M:1/O:0/S:2/ED:1/EE:1/A:1/ID:2/LC:2/LI:1/LAV:1/LAC:1/FD:1/RD:1/NC:2/PV:3)`)
         // console.log(risk);
@@ -44,6 +42,7 @@ const SelectDropDown = (props: any) => {
             <Select
                 id={`dropdown_${props.id}`}
                 value={dropDownData}
+                inputProps={{"name" : props.id}}
                 onChange={handleChange}
                 label={props.name}
             >
@@ -52,7 +51,7 @@ const SelectDropDown = (props: any) => {
                     props.options.map(
                         (data:any, index: number) => (
                             <MenuItem 
-                                value={`${props.id} (${data.value})`} 
+                                value={`${props.id}:${data.value}`} 
                                 key={`options_${props.id}_${index}`}
                             >
                                 {data.name} ({data.value})
