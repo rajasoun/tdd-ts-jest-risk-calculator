d5ebb87fa2b8aa14c36bb4b7dc04e0ecfa196f6b
Arunachalam <arunachalame049@gmail.com>
2021-07-29 20:40:29 +0530

    Material Close Icon

--- src/component/calculator-modal.tsx
+++ src/component/calculator-modal.tsx
@@ -5,13 +5,11 @@ import Grid from '@material-ui/core/Grid';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { createStyles, makeStyles, Theme, useTheme } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
-import { BorderBottom } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
