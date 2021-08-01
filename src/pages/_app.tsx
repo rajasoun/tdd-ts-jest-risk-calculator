808ce2ae8e7acde4f9497a089038e2f6599715ef
Arunachalam <arunachalame049@gmail.com>
2021-08-01 04:56:59 +0000

    build: uI Update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        />
+        <title>OWASP Risk Assessment Calculator v2021</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,95 @@
-import Head from 'next/head';
 import data from '../data/risk-calculator.json';
 
-const Index = () => {
+interface Option {
+    name: string,
+    value: number
+}
 
+interface Select {
+    description: string,
+    name: string,
+    id: number,
+    options: Array<Option>
+}
+
+// Styles for element begins
+const styleTitle = {
+    fontSize: "1.2rem",
+    fontWeight: 600,
+    margin: '1rem'
+}
+
+const styleSelectLabel = {
+    margin: '0rem 1rem',
+    minWidth: 400
+}
+
+const styleSelect = {
+    margin: '0.75rem 1rem',
+    minWidth: 400
+}
+// Styles for element ends
+
+const Index = () => {
     return (
         <>
-            <Head>
-                <title>OWASP Risk Assessment Calculator v2021</title>
-            </Head>
-            <h1 data-testid="header">Risk Assessment Calculator</h1>
+            <h1
+                data-testid="header"
+                className="text-center my-3"
+            >
+                Risk Assessment Calculator
+            </h1>
             <hr />
-            {
-                data.data.map((ele: any, index: number) => (
-                    <div key={`RAC_${index}`}>
+            <div className="container-fluid">
+                <div className="row">
                     {
-                        ele.select.map((data: any) => (
-                            <select key={data.id} data-testid={data.id} name={data.name}>
-                                {
-                                    data.options.map((option: any, i: any) => (
-                                        <option
-                                            key={`${data.name}__${i}`}
-                                            value={option.value}
-                                        >
-                                            {option.name}
-                                        </option>
-                                    ))
-                                }
-                            </select>
+                        data.data.map((ele: any, index: number) => (
+                            <div
+                                className={"col-sm-4"}
+                                key={index}
+                            >
+                                <div
+                                    key={`RAC_${index}`}
+                                    className="col-sm-12"
+                                >
+                                    <div style={styleTitle}>
+                                        {ele.title}
+                                    </div>
+                                    {
+                                        ele.select.map((data: Select) => (
+                                            <div key={data.id}>
+                                                <div className="form-group">
+                                                    <label style={styleSelectLabel}>
+                                                        {data.description}
+                                                    </label>
+                                                    <select
+                                                        data-testid={data.id}
+                                                        name={data.name}
+                                                        className="form-control"
+                                                        style={styleSelect}
+                                                        >
+                                                        {
+                                                            data.options.map(
+                                                                (option: Option, i: number) => (
+                                                                <option
+                                                                    key={`${data.name}__${i}`}
+                                                                    value={option.value}
+                                                                >
+                                                                    {option.name} ({option.value})
+                                                                </option>
+                                                            ))
+                                                        }
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+                            </div>
                         ))
                     }
-                    </div>
-                ))
-            }
+                </div>
+            </div>
         </>
     )
 }
