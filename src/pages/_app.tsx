7fadfeffcd4792212d0b70de8a663137f61a487f
jagadishkesav2 <jagadish@navadhiti.com>
2021-08-10 05:52:59 +0000

    build: change the CDN to npm package to fix the bug issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,15 @@
 import type { AppProps } from 'next/app'
-import Script from 'next/script'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+if (typeof window !== "undefined") {
+  require("jquery");
+  require("bootstrap");
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-        />
-        <Script
-          src="https://code.jquery.com/jquery-3.6.0.min.js"
-        />
-        <Script
-          strategy="lazyOnload"
-          src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-        />
-        <title>OWASP Risk Assessment Calculator v2021</title>
+      <title>OWASP Risk Assessment Calculator v2021</title>
       <Component {...pageProps} />
     </>
   )
